9983ffd671ed7492e58610bb597de4467d1b63a8
mohamed4525 <ma7486635@gmail.com>
2023-11-29 15:17:04 +0200

    adding a README File to the repository

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -9,6 +9,10 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
+from launch.substitutions import Command
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+
 
 
 def generate_launch_description():
@@ -38,10 +42,25 @@ def generate_launch_description():
                         output='screen')
 
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
