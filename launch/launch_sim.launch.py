27bb1940dadca25e8f61c1184f69d6dbabd643c8
mohamed4525 <ma7486635@gmail.com>
2023-12-12 11:32:38 +0200

    12-12-2023--11:30

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -50,8 +50,10 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
 
+             )
+             
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
